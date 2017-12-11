8a1165e580798d7db17bac5db32b297bb46bcb25
Ivo Zinenda <izinenda@gmail.com>
2017-12-12 00:50:03 +0200

    scripts

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$( document ).ready(function(){
+    // materialize plugins
+    $(".button-collapse").sideNav();
+    $('.collapsible').collapsible();
+    $('.materialboxed').materialbox();
+    $('.slider').slider();
+    $('.scrollspy').scrollSpy();
+    // smoothState.js
+    $('#main').smoothState();
+});
