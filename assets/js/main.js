baa65fe5dd63825c3077c7d55d4e8746ff1368d7
Ivo Zinenda <izinenda@gmail.com>
2017-12-16 19:32:52 +0200

    Booking Form

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,19 @@
 $( document ).ready(function(){
     // materialize plugins
     $(".button-collapse").sideNav();
+    $(".modal").modal();
     $('.collapsible').collapsible();
     $('.materialboxed').materialbox();
     $('.slider').slider();
     $('.scrollspy').scrollSpy();
     $('.parallax').parallax();
-    // smoothState.js
-    $('#main').smoothState();
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 10, // Creates a dropdown of 10 years to control year,
+        today: 'Hoje',
+        clear: 'Limpe',
+        close: 'Ok',
+        closeOnSelect: false // Close upon selecting a date,
+    });
+    $('select').material_select();
 });
