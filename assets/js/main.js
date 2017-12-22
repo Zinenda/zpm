dde1559b0d22e9e732937e35212696b7590a4619
Ivo Zinenda <izinenda@gmail.com>
2017-12-22 22:47:07 +0200

    new images

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,7 @@ $( document ).ready(function(){
     // materialize plugins
     $(".button-collapse").sideNav();
     $(".modal").modal();
-    $('.collapsible').collapsible();
-    $('.materialboxed').materialbox();
     $('.slider').slider();
-    $('.scrollspy').scrollSpy();
-    $('.parallax').parallax();
     $('.datepicker').pickadate({
         selectMonths: true, // Creates a dropdown to control month
         selectYears: 10, // Creates a dropdown of 10 years to control year,
